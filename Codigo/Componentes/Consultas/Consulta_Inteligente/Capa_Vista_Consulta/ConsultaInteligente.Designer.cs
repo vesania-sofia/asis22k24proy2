9522b97d6fac1f34ccfd797e451fe8fa3bb70256
sebrix <plunas@miumg.edu.gt>
2024-09-04 10:42:31 -0600

    xd

--- Codigo/Componentes/Consultas/Consulta_Inteligente/Capa_Vista_Consulta/ConsultaInteligente.Designer.cs
+++ Codigo/Componentes/Consultas/Consulta_Inteligente/Capa_Vista_Consulta/ConsultaInteligente.Designer.cs
@@ -29,13 +29,25 @@ namespace Capa_Vista_Consulta
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(549, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ConsultaInteligente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ConsultaInteligente";
             this.Text = "ConsultaInteligente";
             this.Load += new System.EventHandler(this.ConsultaInteligente_Load);
@@ -44,5 +56,7 @@ namespace Capa_Vista_Consulta
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
