8a5f8380918062cba76eb6fb5853afcf99f17f80
CarlosGonzález20 <ll3224645@gmail.com>
2024-09-03 18:21:56 -0600

    Creando solución para consultas inteligentes

---
+++ Codigo/Componentes/Consultas/Consulta_Inteligente/Capa_Modelo_Consulta/consultaSentencias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+
+namespace Capa_Modelo_Consulta
+{
+    public class consultaSentencias
+    {
+    }
+}
---
+++ Codigo/Componentes/Consultas/Consulta_Inteligente/Capa_Vista_Consulta/ConsultaInteligente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Controlador_Consulta;
+
+namespace Capa_Vista_Consulta
+{
+    public partial class ConsultaInteligente : Form
+    {
+        public ConsultaInteligente()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultaInteligente_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
