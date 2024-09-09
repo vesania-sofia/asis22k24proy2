a87bd932d08a92396cfb3591c3f15e82a1dbc4f8
Brandon Boch López <alejandrobochlopez@gmail.com>
2024-09-09 10:08:36 -0600

    Bitacora y Cambio de Contraseña

---
+++ Prototipos/Componentes/Seguridad/CapaDiseno/frm_cambio_contrasenia.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace CapaDiseno
+{
+    partial class frm_cambio_contrasenia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_confirmar = new System.Windows.Forms.Button();
+            this.Txt_confirmacion = new System.Windows.Forms.TextBox();
+            this.Txt_contrasenia = new System.Windows.Forms.TextBox();
+            this.Lbl_confirmacion = new System.Windows.Forms.Label();
+            this.Lbl_contrasenia = new System.Windows.Forms.Label();
+            this.Txt_color = new System.Windows.Forms.TextBox();
+            this.Lbl_pregunta = new System.Windows.Forms.Label();
+            this.Btn_verificar = new System.Windows.Forms.Button();
+            this.Txt_pregunta = new System.Windows.Forms.TextBox();
+            this.Lbl_color = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_confirmar
+            // 
+            this.Btn_confirmar.BackColor = System.Drawing.Color.Firebrick;
+            this.Btn_confirmar.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_confirmar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Btn_confirmar.Location = new System.Drawing.Point(207, 238);
+            this.Btn_confirmar.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_confirmar.Name = "Btn_confirmar";
+            this.Btn_confirmar.Size = new System.Drawing.Size(120, 38);
+            this.Btn_confirmar.TabIndex = 233;
+            this.Btn_confirmar.Tag = "1";
+            this.Btn_confirmar.Text = "Confirmar";
+            this.Btn_confirmar.UseVisualStyleBackColor = false;
+            this.Btn_confirmar.Click += new System.EventHandler(this.Btn_confirmar_Click);
+            // 
+            // Txt_confirmacion
+            // 
+            this.Txt_confirmacion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Txt_confirmacion.Location = new System.Drawing.Point(310, 199);
+            this.Txt_confirmacion.Name = "Txt_confirmacion";
+            this.Txt_confirmacion.Size = new System.Drawing.Size(180, 20);
+            this.Txt_confirmacion.TabIndex = 232;
+            // 
+            // Txt_contrasenia
+            // 
+            this.Txt_contrasenia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Txt_contrasenia.Location = new System.Drawing.Point(310, 157);
+            this.Txt_contrasenia.Name = "Txt_contrasenia";
+            this.Txt_contrasenia.Size = new System.Drawing.Size(180, 20);
+            this.Txt_contrasenia.TabIndex = 231;
+            // 
+            // Lbl_confirmacion
+            // 
+            this.Lbl_confirmacion.AutoSize = true;
+            this.Lbl_confirmacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_confirmacion.Location = new System.Drawing.Point(44, 198);
+            this.Lbl_confirmacion.Name = "Lbl_confirmacion";
+            this.Lbl_confirmacion.Size = new System.Drawing.Size(102, 20);
+            this.Lbl_confirmacion.TabIndex = 230;
+            this.Lbl_confirmacion.Text = "Confirmación";
+            // 
+            // Lbl_contrasenia
+            // 
+            this.Lbl_contrasenia.AutoSize = true;
+            this.Lbl_contrasenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_contrasenia.Location = new System.Drawing.Point(44, 156);
+            this.Lbl_contrasenia.Name = "Lbl_contrasenia";
+            this.Lbl_contrasenia.Size = new System.Drawing.Size(141, 20);
+            this.Lbl_contrasenia.TabIndex = 229;
+            this.Lbl_contrasenia.Text = "Nueva Contraseña";
+            // 
+            // Txt_color
+            // 
+            this.Txt_color.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Txt_color.Location = new System.Drawing.Point(310, 67);
+            this.Txt_color.Name = "Txt_color";
+            this.Txt_color.Size = new System.Drawing.Size(180, 20);
+            this.Txt_color.TabIndex = 228;
+            // 
+            // Lbl_pregunta
+            // 
+            this.Lbl_pregunta.AutoSize = true;
+            this.Lbl_pregunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_pregunta.Location = new System.Drawing.Point(39, 66);
+            this.Lbl_pregunta.Name = "Lbl_pregunta";
+            this.Lbl_pregunta.Size = new System.Drawing.Size(192, 20);
+            this.Lbl_pregunta.TabIndex = 227;
+            this.Lbl_pregunta.Text = "¿Cuál es tu color favorito?";
+            // 
+            // Btn_verificar
+            // 
+            this.Btn_verificar.BackColor = System.Drawing.Color.Firebrick;
+            this.Btn_verificar.Font = new System.Drawing.Font("Candara", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_verificar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Btn_verificar.Location = new System.Drawing.Point(207, 101);
+            this.Btn_verificar.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_verificar.Name = "Btn_verificar";
+            this.Btn_verificar.Size = new System.Drawing.Size(120, 38);
+            this.Btn_verificar.TabIndex = 226;
+            this.Btn_verificar.Tag = "1";
+            this.Btn_verificar.Text = "Verificar";
+            this.Btn_verificar.UseVisualStyleBackColor = false;
+            this.Btn_verificar.Click += new System.EventHandler(this.Btn_verificar_Click);
+            // 
+            // Txt_pregunta
+            // 
+            this.Txt_pregunta.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Txt_pregunta.Location = new System.Drawing.Point(310, 20);
+            this.Txt_pregunta.Name = "Txt_pregunta";
+            this.Txt_pregunta.Size = new System.Drawing.Size(180, 20);
+            this.Txt_pregunta.TabIndex = 225;
+            // 
+            // Lbl_color
+            // 
+            this.Lbl_color.AutoSize = true;
+            this.Lbl_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_color.Location = new System.Drawing.Point(39, 19);
+            this.Lbl_color.Name = "Lbl_color";
+            this.Lbl_color.Size = new System.Drawing.Size(154, 20);
+            this.Lbl_color.TabIndex = 224;
+            this.Lbl_color.Text = "¿Cuál es tu usuario?";
+            // 
+            // frm_cambio_contrasenia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 287);
+            this.Controls.Add(this.Btn_confirmar);
+            this.Controls.Add(this.Txt_confirmacion);
+            this.Controls.Add(this.Txt_contrasenia);
+            this.Controls.Add(this.Lbl_confirmacion);
+            this.Controls.Add(this.Lbl_contrasenia);
+            this.Controls.Add(this.Txt_color);
+            this.Controls.Add(this.Lbl_pregunta);
+            this.Controls.Add(this.Btn_verificar);
+            this.Controls.Add(this.Txt_pregunta);
+            this.Controls.Add(this.Lbl_color);
+            this.Name = "frm_cambio_contrasenia";
+            this.Text = "frm_cambio_contrasenia";
+            this.Load += new System.EventHandler(this.frm_cambio_contrasenia_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_confirmar;
+        private System.Windows.Forms.TextBox Txt_confirmacion;
+        private System.Windows.Forms.TextBox Txt_contrasenia;
+        private System.Windows.Forms.Label Lbl_confirmacion;
+        private System.Windows.Forms.Label Lbl_contrasenia;
+        private System.Windows.Forms.TextBox Txt_color;
+        private System.Windows.Forms.Label Lbl_pregunta;
+        private System.Windows.Forms.Button Btn_verificar;
+        private System.Windows.Forms.TextBox Txt_pregunta;
+        private System.Windows.Forms.Label Lbl_color;
+    }
+}
