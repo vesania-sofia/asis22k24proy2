fe2a9157eb16057d0a31de8cb7b455361336b354
JosueDPG <Josue Paz@LAPTOP-9BKGMKTI>
2024-09-20 20:59:07 -0600

    Cambios en vista de formularios (actualizacion)

--- Codigo/Componentes/Seguridad/CapaDiseno/frm_asignacion_aplicaciones_perfiles.cs
+++ Codigo/Componentes/Seguridad/CapaDiseno/frm_asignacion_aplicaciones_perfiles.cs
@@ -124,140 +124,8 @@ namespace CapaDiseno
         //****** HECHO POR JOSUE PAZ Y KEVIN LOPEZ*************************************
         public static int iContadorFila = 0;
 
-        private void Btn_salir_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        //*******************TRABAJADO POR JOSUE PAZ 0901-21-5560**********************************************************
-        private void Btn_finalizar_Click(object sender, EventArgs e)
-        {
-            string ingresar;
-            string consulta;
-            string modificar;
-            string eliminar;
-            string imprimir;
-            try
-            {
-                foreach (DataGridViewRow Fila in dgv_asignacionesperfiles.Rows)
-                {
-
-                    string codigoperfil = Fila.Cells[0].Value.ToString();
-                    string nombreaplicacion = Fila.Cells[1].Value.ToString();
 
 
-                    bool cheked = ((bool)(Fila.Cells["Ingresar"].EditedFormattedValue));
-                    if (cheked)
-                        ingresar = "1";
-                    else
-                        ingresar = "0";
-
-                    bool chekedM = ((bool)(Fila.Cells["Modificar"].EditedFormattedValue));
-                    if (chekedM)
-                        modificar = "1";
-                    else
-                        modificar = "0";
-
-                    bool chekedE = ((bool)(Fila.Cells["Eliminar"].EditedFormattedValue));
-                    if (chekedE)
-                        eliminar = "1";
-                    else
-                        eliminar = "0";
-
-                    bool chekedC = ((bool)(Fila.Cells["Consultar"].EditedFormattedValue));
-                    if (chekedC)
-                        consulta = "1";
-                    else
-                        consulta = "0";
-
-                    bool chekedI = ((bool)(Fila.Cells["Imprimir"].EditedFormattedValue));
-                    if (chekedI)
-                        imprimir = "1";
-                    else
-                        imprimir = "0";
-
-                    DataTable dtAplicaciones = logic.consultaLogicaPermisosPerfilAplicacion(codigoperfil, nombreaplicacion, ingresar, modificar, eliminar, consulta, imprimir);
-
-                }
-
-                MessageBox.Show("Datos ingresados exitosamente");
-                limpieza();
-                dgv_asignacionesperfiles.Rows.Clear();
-                iContadorFila = 0;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-        }
-        //******************ACA TERMINA********************************************************
-
-        //***************************HECHO POR KEVIN LOPEZ********************************************
-
-        private void Btn_agregar_Click(object sender, EventArgs e)
-        {
-            if (cbo_perfiles.SelectedItem == null || cbo_aplicaciones.SelectedItem == null)
-            {
-                MessageBox.Show("No se ha seleccionado nada aún");
-            }
-            else
-            {
-                bool bUsuario_aplicacion_existente = false;
-
-                string sPerfil = cbo_perfiles.SelectedItem.ToString();
-                string sAplicacion = cbo_aplicaciones.SelectedItem.ToString();
-
-                if (iContadorFila == 0)
-                {
-                    dgv_asignacionesperfiles.Rows.Add(sPerfil, sAplicacion);
-
-                    iContadorFila++;
-
-
-                }
-                else
-                {
-                    foreach (DataGridViewRow Fila in dgv_asignacionesperfiles.Rows)
-                    {
-                        if (Fila.Cells[0].Value.ToString() == cbo_perfiles.SelectedItem.ToString() && Fila.Cells[1].Value.ToString() == cbo_aplicaciones.SelectedItem.ToString())
-                        {
-                            bUsuario_aplicacion_existente = true;
-                        }
-                    }
-
-                    if (bUsuario_aplicacion_existente == true)
-                    {
-                        MessageBox.Show("Ya existe una relacion del Perfil con la aplicacion");
-                    }
-                    else
-                    {
-                        dgv_asignacionesperfiles.Rows.Add(sPerfil, sAplicacion);
-                        iContadorFila++;
-                    }
-                }
-
-                limpieza();
-            }
-        }
-        //***************************HECHO POR KEVIN LOPEZ********************************************
-
-        private void Btn_remover_Click(object sender, EventArgs e)
-        {
-            if (iContadorFila > 0)
-            {
-                dgv_asignacionesperfiles.Rows.RemoveAt(dgv_asignacionesperfiles.CurrentRow.Index);
-                iContadorFila--;
-            }
-            else
-            {
-                MessageBox.Show("No hay relaciones que eliminar");
-            }
-        }
-
-        private void Btn_ayuda_Click(object sender, EventArgs e)
-        {
-            Help.ShowHelp(this, "C:\\Ayuda_Seguridad\\" + "AyudaAsignacionAplicacionesPerfiles.chm", "Asignacion_Aplicaciones_Perfiles.html");
-        }
 
         private void dgv_asignacionesperfiles_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -356,5 +224,146 @@ namespace CapaDiseno
 
         }
 
+
+
+        private void btn_remover_Click_1(object sender, EventArgs e)
+        {
+            if (iContadorFila > 0)
+            {
+                dgv_asignacionesperfiles.Rows.RemoveAt(dgv_asignacionesperfiles.CurrentRow.Index);
+                iContadorFila--;
+            }
+            else
+            {
+                MessageBox.Show("No hay relaciones que eliminar");
+            }
+        }
+
+
+
+        //***************************HECHO POR KEVIN LOPEZ********************************************
+        private void btn_agregar_Click_1(object sender, EventArgs e)
+        {
+            if (cbo_perfiles.SelectedItem == null || cbo_aplicaciones.SelectedItem == null)
+            {
+                MessageBox.Show("No se ha seleccionado nada aún");
+            }
+            else
+            {
+                bool bUsuario_aplicacion_existente = false;
+
+                string sPerfil = cbo_perfiles.SelectedItem.ToString();
+                string sAplicacion = cbo_aplicaciones.SelectedItem.ToString();
+
+                if (iContadorFila == 0)
+                {
+                    dgv_asignacionesperfiles.Rows.Add(sPerfil, sAplicacion);
+
+                    iContadorFila++;
+
+
+                }
+                else
+                {
+                    foreach (DataGridViewRow Fila in dgv_asignacionesperfiles.Rows)
+                    {
+                        if (Fila.Cells[0].Value.ToString() == cbo_perfiles.SelectedItem.ToString() && Fila.Cells[1].Value.ToString() == cbo_aplicaciones.SelectedItem.ToString())
+                        {
+                            bUsuario_aplicacion_existente = true;
+                        }
+                    }
+
+                    if (bUsuario_aplicacion_existente == true)
+                    {
+                        MessageBox.Show("Ya existe una relacion del Perfil con la aplicacion");
+                    }
+                    else
+                    {
+                        dgv_asignacionesperfiles.Rows.Add(sPerfil, sAplicacion);
+                        iContadorFila++;
+                    }
+                }
+
+                limpieza();
+            }
+            //***************************HECHO POR KEVIN LOPEZ********************************************
+        }
+        //*******************TRABAJADO POR JOSUE PAZ 0901-21-5560**********************************************************
+        private void btn_finalizar_Click_1(object sender, EventArgs e)
+        {
+            string ingresar;
+            string consulta;
+            string modificar;
+            string eliminar;
+            string imprimir;
+            try
+            {
+                foreach (DataGridViewRow Fila in dgv_asignacionesperfiles.Rows)
+                {
+
+                    string codigoperfil = Fila.Cells[0].Value.ToString();
+                    string nombreaplicacion = Fila.Cells[1].Value.ToString();
+
+
+                    bool cheked = ((bool)(Fila.Cells["Ingresar"].EditedFormattedValue));
+                    if (cheked)
+                        ingresar = "1";
+                    else
+                        ingresar = "0";
+
+                    bool chekedM = ((bool)(Fila.Cells["Modificar"].EditedFormattedValue));
+                    if (chekedM)
+                        modificar = "1";
+                    else
+                        modificar = "0";
+
+                    bool chekedE = ((bool)(Fila.Cells["Eliminar"].EditedFormattedValue));
+                    if (chekedE)
+                        eliminar = "1";
+                    else
+                        eliminar = "0";
+
+                    bool chekedC = ((bool)(Fila.Cells["Consultar"].EditedFormattedValue));
+                    if (chekedC)
+                        consulta = "1";
+                    else
+                        consulta = "0";
+
+                    bool chekedI = ((bool)(Fila.Cells["Imprimir"].EditedFormattedValue));
+                    if (chekedI)
+                        imprimir = "1";
+                    else
+                        imprimir = "0";
+
+                    DataTable dtAplicaciones = logic.consultaLogicaPermisosPerfilAplicacion(codigoperfil, nombreaplicacion, ingresar, modificar, eliminar, consulta, imprimir);
+
+                }
+
+                MessageBox.Show("Datos ingresados exitosamente");
+                limpieza();
+                dgv_asignacionesperfiles.Rows.Clear();
+                iContadorFila = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private void btn_salir_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_buscar_Click_1(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+
+        private void btn_ayuda_Click_1(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, "C:\\Ayuda_Seguridad\\" + "AyudaAsignacionAplicacionesPerfiles.chm", "Asignacion_Aplicaciones_Perfiles.html");
+        }
     }
+    //******************ACA TERMINA********************************************************
 }
