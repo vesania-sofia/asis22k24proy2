95d947c16b68e7eab7c7c761e9f7ea8894e3a646
sebas-luna <plunas@miumg.edu.gt>
2024-09-23 11:29:09 -0600

    Iconos nuevos

--- Codigo/Componentes/Consultas/Capa_Vista_Consulta/ConsultaSimple.Designer.cs
+++ Codigo/Componentes/Consultas/Capa_Vista_Consulta/ConsultaSimple.Designer.cs
@@ -45,12 +45,12 @@ namespace Capa_Vista_Consulta
             // dgvConsultar2
             // 
             this.dgvConsultar2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvConsultar2.Location = new System.Drawing.Point(25, 99);
-            this.dgvConsultar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvConsultar2.Location = new System.Drawing.Point(19, 80);
+            this.dgvConsultar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvConsultar2.Name = "dgvConsultar2";
             this.dgvConsultar2.RowHeadersWidth = 51;
             this.dgvConsultar2.RowTemplate.Height = 24;
-            this.dgvConsultar2.Size = new System.Drawing.Size(769, 208);
+            this.dgvConsultar2.Size = new System.Drawing.Size(577, 169);
             this.dgvConsultar2.TabIndex = 35;
             // 
             // btnCancelar2
@@ -58,10 +58,10 @@ namespace Capa_Vista_Consulta
             this.btnCancelar2.FlatAppearance.BorderSize = 0;
             this.btnCancelar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar2.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar2.Image")));
-            this.btnCancelar2.Location = new System.Drawing.Point(669, 16);
-            this.btnCancelar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelar2.Location = new System.Drawing.Point(502, 13);
+            this.btnCancelar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancelar2.Name = "btnCancelar2";
-            this.btnCancelar2.Size = new System.Drawing.Size(85, 66);
+            this.btnCancelar2.Size = new System.Drawing.Size(64, 54);
             this.btnCancelar2.TabIndex = 38;
             this.btnCancelar2.UseVisualStyleBackColor = true;
             // 
@@ -70,69 +70,75 @@ namespace Capa_Vista_Consulta
             this.btnConsultar2.FlatAppearance.BorderSize = 0;
             this.btnConsultar2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConsultar2.Image = ((System.Drawing.Image)(resources.GetObject("btnConsultar2.Image")));
-            this.btnConsultar2.Location = new System.Drawing.Point(541, 16);
-            this.btnConsultar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConsultar2.Location = new System.Drawing.Point(406, 13);
+            this.btnConsultar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConsultar2.Name = "btnConsultar2";
-            this.btnConsultar2.Size = new System.Drawing.Size(93, 66);
+            this.btnConsultar2.Size = new System.Drawing.Size(70, 54);
             this.btnConsultar2.TabIndex = 36;
             this.btnConsultar2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 28);
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 39;
             this.label1.Text = "Campo";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(204, 28);
+            this.label2.Location = new System.Drawing.Point(153, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 40;
             this.label2.Text = "Operación";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(322, 28);
+            this.label3.Location = new System.Drawing.Point(242, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 41;
             this.label3.Text = "Valor";
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(325, 60);
+            this.txtValor.Location = new System.Drawing.Point(244, 49);
+            this.txtValor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(169, 22);
+            this.txtValor.Size = new System.Drawing.Size(128, 20);
             this.txtValor.TabIndex = 42;
             // 
             // cboCampo
             // 
             this.cboCampo.FormattingEnabled = true;
-            this.cboCampo.Location = new System.Drawing.Point(25, 58);
+            this.cboCampo.Location = new System.Drawing.Point(19, 47);
+            this.cboCampo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboCampo.Name = "cboCampo";
-            this.cboCampo.Size = new System.Drawing.Size(146, 24);
+            this.cboCampo.Size = new System.Drawing.Size(110, 21);
             this.cboCampo.TabIndex = 43;
             // 
             // cboOperador
             // 
             this.cboOperador.FormattingEnabled = true;
-            this.cboOperador.Location = new System.Drawing.Point(207, 58);
+            this.cboOperador.Location = new System.Drawing.Point(155, 47);
+            this.cboOperador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cboOperador.Name = "cboOperador";
-            this.cboOperador.Size = new System.Drawing.Size(71, 24);
+            this.cboOperador.Size = new System.Drawing.Size(54, 21);
             this.cboOperador.TabIndex = 44;
             // 
             // ConsultaSimple
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(815, 324);
+            this.ClientSize = new System.Drawing.Size(611, 263);
             this.Controls.Add(this.cboOperador);
             this.Controls.Add(this.cboCampo);
             this.Controls.Add(this.txtValor);
@@ -142,7 +148,7 @@ namespace Capa_Vista_Consulta
             this.Controls.Add(this.dgvConsultar2);
             this.Controls.Add(this.btnCancelar2);
             this.Controls.Add(this.btnConsultar2);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ConsultaSimple";
             this.Text = "ConsultaSimple";
             this.Load += new System.EventHandler(this.ConsultaSimple_Load);
