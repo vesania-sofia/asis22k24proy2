48821443c131f1280ae851b16f3dc7ec9ecb4ffe
Angel <aajanelt1@miumg.edu.gt>
2024-09-27 15:24:11 -0600

    modificaciones estandarización de colores capa vista reporteador

--- Codigo/Componentes/Reporteador/Capa_Vista_Reporteria/menu_reporteria.Designer.cs
+++ Codigo/Componentes/Reporteador/Capa_Vista_Reporteria/menu_reporteria.Designer.cs
@@ -31,12 +31,12 @@ namespace Capa_Vista_Reporteria
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu_reporteria));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Btn_Ayuda = new System.Windows.Forms.Button();
             this.Pic_1 = new System.Windows.Forms.PictureBox();
             this.Btn_RepUsuario = new System.Windows.Forms.Button();
             this.Btn_RepAdmin = new System.Windows.Forms.Button();
             this.Pic_2 = new System.Windows.Forms.PictureBox();
             this.Pic_Foto = new System.Windows.Forms.PictureBox();
-            this.Btn_Ayuda = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Pic_1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Pic_2)).BeginInit();
@@ -45,6 +45,7 @@ namespace Capa_Vista_Reporteria
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(177)))), ((int)(((byte)(252)))));
             this.flowLayoutPanel1.Controls.Add(this.Btn_Ayuda);
             this.flowLayoutPanel1.Controls.Add(this.Pic_1);
             this.flowLayoutPanel1.Controls.Add(this.Btn_RepUsuario);
@@ -53,28 +54,48 @@ namespace Capa_Vista_Reporteria
             this.flowLayoutPanel1.Controls.Add(this.Pic_Foto);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(185, 591);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(247, 727);
             this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Btn_Ayuda
+            // 
+            this.Btn_Ayuda.BackColor = System.Drawing.Color.Transparent;
+            this.Btn_Ayuda.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Ayuda.FlatAppearance.BorderSize = 0;
+            this.Btn_Ayuda.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_Ayuda.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Btn_Ayuda.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Ayuda.Image")));
+            this.Btn_Ayuda.Location = new System.Drawing.Point(4, 4);
+            this.Btn_Ayuda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Ayuda.Name = "Btn_Ayuda";
+            this.Btn_Ayuda.Size = new System.Drawing.Size(58, 52);
+            this.Btn_Ayuda.TabIndex = 3;
+            this.Btn_Ayuda.UseVisualStyleBackColor = false;
+            this.Btn_Ayuda.Click += new System.EventHandler(this.Btn_Ayuda_Click);
             // 
             // Pic_1
             // 
             this.Pic_1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Pic_1.Location = new System.Drawing.Point(3, 51);
+            this.Pic_1.Location = new System.Drawing.Point(4, 64);
+            this.Pic_1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Pic_1.Name = "Pic_1";
-            this.Pic_1.Size = new System.Drawing.Size(182, 155);
+            this.Pic_1.Size = new System.Drawing.Size(243, 191);
             this.Pic_1.TabIndex = 2;
             this.Pic_1.TabStop = false;
             // 
             // Btn_RepUsuario
             // 
+            this.Btn_RepUsuario.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Btn_RepUsuario.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Btn_RepUsuario.FlatAppearance.BorderSize = 0;
+            this.Btn_RepUsuario.FlatAppearance.BorderSize = 2;
             this.Btn_RepUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Btn_RepUsuario.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
-            this.Btn_RepUsuario.Location = new System.Drawing.Point(3, 212);
+            this.Btn_RepUsuario.Location = new System.Drawing.Point(4, 263);
+            this.Btn_RepUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Btn_RepUsuario.Name = "Btn_RepUsuario";
-            this.Btn_RepUsuario.Size = new System.Drawing.Size(185, 40);
+            this.Btn_RepUsuario.Size = new System.Drawing.Size(240, 72);
             this.Btn_RepUsuario.TabIndex = 0;
             this.Btn_RepUsuario.Text = "Reporteria Usuarios";
             this.Btn_RepUsuario.UseVisualStyleBackColor = true;
@@ -82,13 +103,15 @@ namespace Capa_Vista_Reporteria
             // 
             // Btn_RepAdmin
             // 
+            this.Btn_RepAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Btn_RepAdmin.Dock = System.Windows.Forms.DockStyle.Top;
-            this.Btn_RepAdmin.FlatAppearance.BorderSize = 0;
+            this.Btn_RepAdmin.FlatAppearance.BorderSize = 2;
             this.Btn_RepAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Btn_RepAdmin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_RepAdmin.Location = new System.Drawing.Point(3, 258);
+            this.Btn_RepAdmin.Location = new System.Drawing.Point(4, 343);
+            this.Btn_RepAdmin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Btn_RepAdmin.Name = "Btn_RepAdmin";
-            this.Btn_RepAdmin.Size = new System.Drawing.Size(185, 67);
+            this.Btn_RepAdmin.Size = new System.Drawing.Size(240, 82);
             this.Btn_RepAdmin.TabIndex = 1;
             this.Btn_RepAdmin.Text = "Reporteria Administrador";
             this.Btn_RepAdmin.UseVisualStyleBackColor = true;
@@ -98,10 +121,11 @@ namespace Capa_Vista_Reporteria
             // 
             this.Pic_2.Dock = System.Windows.Forms.DockStyle.Top;
             this.Pic_2.Image = ((System.Drawing.Image)(resources.GetObject("Pic_2.Image")));
-            this.Pic_2.Location = new System.Drawing.Point(3, 331);
+            this.Pic_2.Location = new System.Drawing.Point(4, 433);
+            this.Pic_2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Pic_2.Name = "Pic_2";
-            this.Pic_2.Size = new System.Drawing.Size(182, 110);
-            this.Pic_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic_2.Size = new System.Drawing.Size(243, 135);
+            this.Pic_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Pic_2.TabIndex = 3;
             this.Pic_2.TabStop = false;
             // 
@@ -109,32 +133,23 @@ namespace Capa_Vista_Reporteria
             // 
             this.Pic_Foto.Dock = System.Windows.Forms.DockStyle.Top;
             this.Pic_Foto.Image = global::Capa_Vista_Reporteria.Properties.Resources.ssssss;
-            this.Pic_Foto.Location = new System.Drawing.Point(3, 447);
+            this.Pic_Foto.Location = new System.Drawing.Point(4, 576);
+            this.Pic_Foto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Pic_Foto.Name = "Pic_Foto";
-            this.Pic_Foto.Size = new System.Drawing.Size(182, 141);
+            this.Pic_Foto.Size = new System.Drawing.Size(243, 174);
             this.Pic_Foto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Pic_Foto.TabIndex = 4;
             this.Pic_Foto.TabStop = false;
             // 
-            // Btn_Ayuda
-            // 
-            this.Btn_Ayuda.BackColor = System.Drawing.Color.White;
-            this.Btn_Ayuda.Image = global::Capa_Vista_Reporteria.Properties.Resources.ayuda;
-            this.Btn_Ayuda.Location = new System.Drawing.Point(3, 3);
-            this.Btn_Ayuda.Name = "Btn_Ayuda";
-            this.Btn_Ayuda.Size = new System.Drawing.Size(50, 42);
-            this.Btn_Ayuda.TabIndex = 3;
-            this.Btn_Ayuda.UseVisualStyleBackColor = false;
-            this.Btn_Ayuda.Click += new System.EventHandler(this.Btn_Ayuda_Click);
-            // 
             // menu_reporteria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(247)))), ((int)(((byte)(253)))));
-            this.ClientSize = new System.Drawing.Size(961, 591);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1281, 727);
             this.Controls.Add(this.flowLayoutPanel1);
             this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "menu_reporteria";
             this.Text = "menu_reporteria";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
