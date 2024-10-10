2e2e4f924c666d4653290e754b61bb361feaeeea
Ammy C <Ammy C@AMMY>
2024-10-10 08:58:51 -0600

    Se creo el proyecto de logistica y los 3 formularios de transporte

---
+++ Codigo/Modulos/Logistica/Capa_Vista_Logistica/Cliente.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Capa_Vista_Logistica
+{
+    partial class Cliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datos_Cliente = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Txt_Identificacion = new System.Windows.Forms.TextBox();
+            this.Txt_Nombre = new System.Windows.Forms.TextBox();
+            this.Txt_Direccion = new System.Windows.Forms.TextBox();
+            this.Txt_Telefono = new System.Windows.Forms.TextBox();
+            this.Txt_Correo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Txt_Cliente = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.No_Identificación = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Datos_Cliente
+            // 
+            this.Datos_Cliente.AutoSize = true;
+            this.Datos_Cliente.Location = new System.Drawing.Point(378, 33);
+            this.Datos_Cliente.Name = "Datos_Cliente";
+            this.Datos_Cliente.Size = new System.Drawing.Size(92, 17);
+            this.Datos_Cliente.TabIndex = 0;
+            this.Datos_Cliente.Text = "Datos Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "No. Identificación";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cliente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(446, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Dirección";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(446, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Télefono";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(401, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Correo_Electronico";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Txt_Identificacion
+            // 
+            this.Txt_Identificacion.Location = new System.Drawing.Point(177, 155);
+            this.Txt_Identificacion.Name = "Txt_Identificacion";
+            this.Txt_Identificacion.Size = new System.Drawing.Size(134, 22);
+            this.Txt_Identificacion.TabIndex = 9;
+            this.Txt_Identificacion.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Txt_Nombre
+            // 
+            this.Txt_Nombre.Location = new System.Drawing.Point(177, 218);
+            this.Txt_Nombre.Name = "Txt_Nombre";
+            this.Txt_Nombre.Size = new System.Drawing.Size(134, 22);
+            this.Txt_Nombre.TabIndex = 10;
+            // 
+            // Txt_Direccion
+            // 
+            this.Txt_Direccion.Location = new System.Drawing.Point(562, 95);
+            this.Txt_Direccion.Name = "Txt_Direccion";
+            this.Txt_Direccion.Size = new System.Drawing.Size(140, 22);
+            this.Txt_Direccion.TabIndex = 11;
+            // 
+            // Txt_Telefono
+            // 
+            this.Txt_Telefono.Location = new System.Drawing.Point(562, 155);
+            this.Txt_Telefono.Name = "Txt_Telefono";
+            this.Txt_Telefono.Size = new System.Drawing.Size(140, 22);
+            this.Txt_Telefono.TabIndex = 12;
+            // 
+            // Txt_Correo
+            // 
+            this.Txt_Correo.Location = new System.Drawing.Point(562, 213);
+            this.Txt_Correo.Name = "Txt_Correo";
+            this.Txt_Correo.Size = new System.Drawing.Size(140, 22);
+            this.Txt_Correo.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.Txt_Cliente);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(59, 300);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(772, 87);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Buscar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Cliente";
+            // 
+            // Txt_Cliente
+            // 
+            this.Txt_Cliente.Location = new System.Drawing.Point(127, 42);
+            this.Txt_Cliente.Name = "Txt_Cliente";
+            this.Txt_Cliente.Size = new System.Drawing.Size(459, 22);
+            this.Txt_Cliente.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Telefono,
+            this.Correo,
+            this.No_Identificación,
+            this.C,
+            this.Dir});
+            this.dataGridView1.Location = new System.Drawing.Point(59, 440);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(807, 190);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.Width = 125;
+            // 
+            // Telefono
+            // 
+            this.Telefono.HeaderText = "Telefono";
+            this.Telefono.MinimumWidth = 6;
+            this.Telefono.Name = "Telefono";
+            this.Telefono.Width = 125;
+            // 
+            // Correo
+            // 
+            this.Correo.HeaderText = "Correo_Electronico";
+            this.Correo.MinimumWidth = 6;
+            this.Correo.Name = "Correo";
+            this.Correo.Width = 125;
+            // 
+            // No_Identificación
+            // 
+            this.No_Identificación.HeaderText = "No_Identificación";
+            this.No_Identificación.MinimumWidth = 6;
+            this.No_Identificación.Name = "No_Identificación";
+            this.No_Identificación.Width = 125;
+            // 
+            // C
+            // 
+            this.C.HeaderText = "Cliente";
+            this.C.MinimumWidth = 6;
+            this.C.Name = "C";
+            this.C.Width = 125;
+            // 
+            // Dir
+            // 
+            this.Dir.HeaderText = "Dirección";
+            this.Dir.MinimumWidth = 6;
+            this.Dir.Name = "Dir";
+            this.Dir.Width = 125;
+            // 
+            // Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 695);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Txt_Correo);
+            this.Controls.Add(this.Txt_Telefono);
+            this.Controls.Add(this.Txt_Direccion);
+            this.Controls.Add(this.Txt_Nombre);
+            this.Controls.Add(this.Txt_Identificacion);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Datos_Cliente);
+            this.Name = "Cliente";
+            this.Text = "Cliente";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Datos_Cliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Txt_Identificacion;
+        private System.Windows.Forms.TextBox Txt_Nombre;
+        private System.Windows.Forms.TextBox Txt_Direccion;
+        private System.Windows.Forms.TextBox Txt_Telefono;
+        private System.Windows.Forms.TextBox Txt_Correo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox Txt_Cliente;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Correo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No_Identificación;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dir;
+    }
+}
