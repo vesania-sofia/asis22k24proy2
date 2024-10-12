bcb1f840e85aa44ff1c94d7425afa6f61d21ac69
Javier Chamo <Javier-cyber-Wx>
2024-10-12 17:51:49 -0600

    agregando formularios de mantenimiento y movimiento de inventarios

---
+++ Codigo/Modulos/Logistica/Capa_Vista_Logistica/Movimiento de Inventario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class Movimiento_de_Inventario : Form
+    {
+        public Movimiento_de_Inventario()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Movimiento_de_Inventario_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
