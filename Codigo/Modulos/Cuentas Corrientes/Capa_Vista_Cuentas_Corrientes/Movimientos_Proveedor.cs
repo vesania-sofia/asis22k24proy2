f8f550cf0fbfa5c16259d8492b5e1e3e1aeb6a0c
Elvir <esandovalp5@miumg.edu.gt>
2024-10-13 20:33:42 -0600

    Prototipo movimiento proveedores

--- Codigo/Modulos/Cuentas Corrientes/Capa_Vista_Cuentas_Corrientes/Movimientos_Proveedor.cs
+++ Codigo/Modulos/Cuentas Corrientes/Capa_Vista_Cuentas_Corrientes/Movimientos_Proveedor.cs
@@ -16,5 +16,15 @@ namespace Capa_Vista_Cuentas_Corrientes
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
