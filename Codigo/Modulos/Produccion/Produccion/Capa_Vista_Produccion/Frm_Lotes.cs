810da6067e35109d8416c4c8818e259fc73e8241
jorge <ja605119@gamail.com>
2024-10-20 12:37:19 -0600

    Modificacion en Series y Lotes

--- Codigo/Modulos/Produccion/Produccion/Capa_Vista_Produccion/Frm_Lotes.cs
+++ Codigo/Modulos/Produccion/Produccion/Capa_Vista_Produccion/Frm_Lotes.cs
@@ -15,6 +15,22 @@ namespace Capa_Vista_Produccion
         public Frm_Lotes()
         {
             InitializeComponent();
+
+            string[] alias = { "ID Lote", "Codigo ", "Producto", "Cantidad", "Fecha Creacion", "Fecha Expiracion", "Etapa", "Descripcion", "Estado" };
+            navegador1.AsignarAlias(alias);
+            navegador1.AsignarSalida(this);
+            // Instanciar colores personalizados
+            Color colorFondo = ColorTranslator.FromHtml("#AEA1D6"); // Color de fondo
+            Color colorFuente = ColorTranslator.FromHtml("#230050"); // Color de fuente
+            navegador1.AsignarColorFondo(colorFondo);
+            navegador1.AsignarColorFuente(colorFuente);
+            navegador1.ObtenerIdAplicacion("1000");
+            navegador1.AsignarAyuda("1");
+            navegador1.ObtenerIdUsuario("admin");
+            navegador1.AsignarTabla("tbl_lotes");
+            navegador1.AsignarNombreForm("Lotes");
+            // List<string> tablas = new List<string> { "otra_tabla_1", "otra_tabla_2" };
+            // navegador1.AsignarTablas(tablas); 
         }
     }
 }
