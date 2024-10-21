fbd9a63adebf7cbbe46d4c0f90a8b5c4b2ed0725
JdanielS7 <jsierrac3@miumg.edu.gt>
2024-10-21 15:44:59 -0600

    Funcionalidad para el form de MonitoreoAlmacen

--- Codigo/Modulos/Logistica/Capa_Vista_Logistica/Almacenes.cs
+++ Codigo/Modulos/Logistica/Capa_Vista_Logistica/Almacenes.cs
@@ -29,24 +29,10 @@ namespace Capa_Vista_Logistica
             navegador1.AsignarAyuda("1");
             navegador1.AsignarNombreForm("ALMACEN");
 
-            // Configurar el DataGridView para que responda a clics en las celdas
-            navegador1.Dgv_Informacion.CellClick += DgvAlmacenes_CellClick; // Asegúrate de que DgvAlmacenes esté definido
+            
         }
 
-        // Evento que se dispara al hacer clic en una celda del DataGridView
-        private void DgvAlmacenes_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            // Verificar que la fila seleccionada es válida
-            if (e.RowIndex >= 0)
-            {
-                // Obtener datos de la fila seleccionada, por ejemplo, el ID de la bodega
-                DataGridViewRow row = navegador1.Dgv_Informacion.Rows[e.RowIndex];
-                string idBodega = row.Cells["Pk_ID_BODEGA"].Value.ToString();  // Asegúrate de que el nombre del campo sea correcto
-
-                // Abrir el formulario MonitoreoAlmacen y pasarle el ID de la bodega
-                MonitoreoAlmacen monitoreoForm = new MonitoreoAlmacen(idBodega);
-                monitoreoForm.Show();
-            }
+        
         }
     }
-}
+
