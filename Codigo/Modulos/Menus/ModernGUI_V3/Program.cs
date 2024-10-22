eae7cb26f1d9f80c316f1b304d3e7558269b0cc4
kgomezt2@miumg.edu.gt <kgomezt2@miumg.edu.gt>
2024-10-21 19:00:20 -0600

    pull

---
+++ Codigo/Modulos/Menus/ModernGUI_V3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfac_V3           
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new splash());
+            //Application.Run(new FormModulos());
+            //Application.Run(new Capa_Vista_Seguridad.frm_login());
+
+        }
+    }
+}
