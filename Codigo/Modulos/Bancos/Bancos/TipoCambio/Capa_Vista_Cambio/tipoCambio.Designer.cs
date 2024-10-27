e15c6dcd7f33c14f8ada67f64b38c9e0471f7164
CarlosGonzález20 <ll3224645@gmail.com>
2024-10-27 17:06:51 -0600

    agregando estructura mvc para frm tipo cambio, así como agregando vista.

---
+++ Codigo/Modulos/Bancos/Bancos/TipoCambio/Capa_Vista_Cambio/tipoCambio.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Capa_Vista_Cambio
+{
+    partial class tipoCambio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboMonedas = new System.Windows.Forms.ComboBox();
+            this.dgvTipoCambio = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCambio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboMonedas
+            // 
+            this.cboMonedas.FormattingEnabled = true;
+            this.cboMonedas.Location = new System.Drawing.Point(376, 87);
+            this.cboMonedas.Name = "cboMonedas";
+            this.cboMonedas.Size = new System.Drawing.Size(141, 24);
+            this.cboMonedas.TabIndex = 10;
+            // 
+            // dgvTipoCambio
+            // 
+            this.dgvTipoCambio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTipoCambio.Location = new System.Drawing.Point(27, 23);
+            this.dgvTipoCambio.Name = "dgvTipoCambio";
+            this.dgvTipoCambio.RowHeadersWidth = 51;
+            this.dgvTipoCambio.RowTemplate.Height = 24;
+            this.dgvTipoCambio.Size = new System.Drawing.Size(318, 307);
+            this.dgvTipoCambio.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(373, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seleecionar una moneda";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(473, 138);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 7;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(557, 87);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // tipoCambio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 350);
+            this.Controls.Add(this.cboMonedas);
+            this.Controls.Add(this.dgvTipoCambio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "tipoCambio";
+            this.Text = "tipoCambio";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCambio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboMonedas;
+        private System.Windows.Forms.DataGridView dgvTipoCambio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
