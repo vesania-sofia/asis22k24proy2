ab7e2a72a176e167d783b8b0332cdcc37c2851e9
JvCas10 <jcastellanosp3@miumg.edu.gt>
2024-10-29 12:58:04 -0600

    Agregando Estados Financieros implementado con el MDI

---
+++ Codigo/Modulos/Contabilidad/EstadosFinancieros/Capa_Vista_EstadosFinancieros/EstadosFinancieros.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Capa_Vista_EstadosFinancieros
+{
+    partial class EstadosFinancieros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Ver_BalanceHistorico = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Ver_Balance = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Ver_EResultadosHistorico = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_VerEstadoResultados = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_Ver_FEfectivoHistorico = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_VerFlujo = new System.Windows.Forms.Button();
+            this.btn_Ver_Ayuda = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Ver_BalanceHistorico);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btn_Ver_Balance);
+            this.groupBox1.Location = new System.Drawing.Point(41, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 145);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Balance General";
+            // 
+            // btn_Ver_BalanceHistorico
+            // 
+            this.btn_Ver_BalanceHistorico.Location = new System.Drawing.Point(196, 61);
+            this.btn_Ver_BalanceHistorico.Name = "btn_Ver_BalanceHistorico";
+            this.btn_Ver_BalanceHistorico.Size = new System.Drawing.Size(75, 28);
+            this.btn_Ver_BalanceHistorico.TabIndex = 6;
+            this.btn_Ver_BalanceHistorico.Text = "Ver ";
+            this.btn_Ver_BalanceHistorico.UseVisualStyleBackColor = true;
+            this.btn_Ver_BalanceHistorico.Click += new System.EventHandler(this.btn_Ver_BalanceHistorico_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Balance Historico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Balance Actual";
+            // 
+            // btn_Ver_Balance
+            // 
+            this.btn_Ver_Balance.Location = new System.Drawing.Point(33, 61);
+            this.btn_Ver_Balance.Name = "btn_Ver_Balance";
+            this.btn_Ver_Balance.Size = new System.Drawing.Size(75, 28);
+            this.btn_Ver_Balance.TabIndex = 0;
+            this.btn_Ver_Balance.Text = "Ver ";
+            this.btn_Ver_Balance.UseVisualStyleBackColor = true;
+            this.btn_Ver_Balance.Click += new System.EventHandler(this.btn_Ver_Balance_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.btn_Ver_EResultadosHistorico);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btn_VerEstadoResultados);
+            this.groupBox2.Location = new System.Drawing.Point(41, 155);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(325, 145);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Estado de resultados";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "E. de Resultados Historico";
+            // 
+            // btn_Ver_EResultadosHistorico
+            // 
+            this.btn_Ver_EResultadosHistorico.Location = new System.Drawing.Point(196, 83);
+            this.btn_Ver_EResultadosHistorico.Name = "btn_Ver_EResultadosHistorico";
+            this.btn_Ver_EResultadosHistorico.Size = new System.Drawing.Size(75, 28);
+            this.btn_Ver_EResultadosHistorico.TabIndex = 8;
+            this.btn_Ver_EResultadosHistorico.Text = "Ver ";
+            this.btn_Ver_EResultadosHistorico.UseVisualStyleBackColor = true;
+            this.btn_Ver_EResultadosHistorico.Click += new System.EventHandler(this.btn_Ver_EResultadosHistorico_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "E. de Resultados Actual";
+            // 
+            // btn_VerEstadoResultados
+            // 
+            this.btn_VerEstadoResultados.Location = new System.Drawing.Point(33, 83);
+            this.btn_VerEstadoResultados.Name = "btn_VerEstadoResultados";
+            this.btn_VerEstadoResultados.Size = new System.Drawing.Size(75, 28);
+            this.btn_VerEstadoResultados.TabIndex = 1;
+            this.btn_VerEstadoResultados.Text = "Ver ";
+            this.btn_VerEstadoResultados.UseVisualStyleBackColor = true;
+            this.btn_VerEstadoResultados.Click += new System.EventHandler(this.btn_VerEstadoResultados_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.btn_Ver_FEfectivoHistorico);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.btn_VerFlujo);
+            this.groupBox3.Location = new System.Drawing.Point(41, 304);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(325, 145);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Flujo de efectivo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(183, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "F. de Efectivo Historico";
+            // 
+            // btn_Ver_FEfectivoHistorico
+            // 
+            this.btn_Ver_FEfectivoHistorico.Location = new System.Drawing.Point(196, 92);
+            this.btn_Ver_FEfectivoHistorico.Name = "btn_Ver_FEfectivoHistorico";
+            this.btn_Ver_FEfectivoHistorico.Size = new System.Drawing.Size(75, 28);
+            this.btn_Ver_FEfectivoHistorico.TabIndex = 9;
+            this.btn_Ver_FEfectivoHistorico.Text = "Ver ";
+            this.btn_Ver_FEfectivoHistorico.UseVisualStyleBackColor = true;
+            this.btn_Ver_FEfectivoHistorico.Click += new System.EventHandler(this.btn_Ver_FEfectivoHistorico_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "F. de Efectivo Actual";
+            // 
+            // btn_VerFlujo
+            // 
+            this.btn_VerFlujo.Location = new System.Drawing.Point(33, 92);
+            this.btn_VerFlujo.Name = "btn_VerFlujo";
+            this.btn_VerFlujo.Size = new System.Drawing.Size(75, 28);
+            this.btn_VerFlujo.TabIndex = 1;
+            this.btn_VerFlujo.Text = "Ver ";
+            this.btn_VerFlujo.UseVisualStyleBackColor = true;
+            this.btn_VerFlujo.Click += new System.EventHandler(this.btn_VerFlujo_Click);
+            // 
+            // btn_Ver_Ayuda
+            // 
+            this.btn_Ver_Ayuda.Image = global::Capa_Vista_EstadosFinancieros.Properties.Resources.AYUDA_V4;
+            this.btn_Ver_Ayuda.Location = new System.Drawing.Point(393, 12);
+            this.btn_Ver_Ayuda.Name = "btn_Ver_Ayuda";
+            this.btn_Ver_Ayuda.Size = new System.Drawing.Size(59, 55);
+            this.btn_Ver_Ayuda.TabIndex = 6;
+            this.btn_Ver_Ayuda.UseVisualStyleBackColor = true;
+            this.btn_Ver_Ayuda.Click += new System.EventHandler(this.btn_Ver_Ayuda_Click);
+            // 
+            // EstadosFinancieros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
+            this.ClientSize = new System.Drawing.Size(464, 481);
+            this.Controls.Add(this.btn_Ver_Ayuda);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "EstadosFinancieros";
+            this.Text = "EstadosFinancieros";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_Ver_Balance;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_VerFlujo;
+        private System.Windows.Forms.Button btn_VerEstadoResultados;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Ver_BalanceHistorico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Ver_EResultadosHistorico;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_Ver_FEfectivoHistorico;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_Ver_Ayuda;
+    }
+}
---
+++ Codigo/Modulos/Contabilidad/EstadosFinancieros/Capa_Vista_EstadosFinancieros/EstadosFinancieros.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using CrystalDecisions.Windows.Forms;
+
+
+
+namespace Capa_Vista_EstadosFinancieros
+{
+    public partial class EstadosFinancieros : Form
+    {
+        string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+
+        public EstadosFinancieros()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ver_Balance_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "Balance.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_VerFlujo_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "FlujoDeEfectivo.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_VerEstadoResultados_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "EstadoDeResultados.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_Ver_BalanceHistorico_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "BalanceHistorico.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_Ver_EResultadosHistorico_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "EstadoDeResultadosHistorico.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_Ver_FEfectivoHistorico_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del reporte
+            ReportDocument reporte = new ReportDocument();
+
+            // Retroceder varios niveles desde el directorio base de la aplicación
+            //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            string sRutaReporte = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "FlujoDeEfectivoHistorico.rpt");
+
+            try
+            {
+                // Mostrar la ruta en un MessageBox para verificar
+                MessageBox.Show("Ruta del reporte: " + sRutaReporte, "Ruta Generada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Cargar el reporte desde la ruta especificada
+                reporte.Load(sRutaReporte);
+
+                // Mostrar el reporte en un formulario de Crystal Report Viewer
+                Form viewerForm = new Form();
+                CrystalReportViewer viewer = new CrystalReportViewer();
+                viewer.Dock = DockStyle.Fill;
+                viewer.ReportSource = reporte;
+                viewerForm.Controls.Add(viewer);
+                viewerForm.WindowState = FormWindowState.Maximized;
+                viewerForm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el reporte: " + ex.Message);
+            }
+            finally
+            {
+                // Libera los recursos del reporte
+                reporte.Close();
+                reporte.Dispose();
+            }
+        }
+
+        private void btn_Ver_Ayuda_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //string sRutaProyecto = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+                string sAyudaPath = Path.Combine(sRutaProyecto, "Contabilidad", "EstadosFinancieros", "ReportesEstados", "AyudaEstadosFinancieros.chm");
+                //string sIndiceAyuda = Path.Combine(sRutaProyecto, "EstadosFinancieros", "ReportesEstados", "Htmlayuda.hmtl");
+
+                Help.ShowHelp(this, sAyudaPath, "Htmlayuda.html");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                // Mostrar un mensaje de error en caso de una excepción
+                MessageBox.Show("Ocurrió un error al abrir la ayuda: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine("Error al abrir la ayuda: " + ex.ToString());
+            }
+
+        }
+    } 
+}
