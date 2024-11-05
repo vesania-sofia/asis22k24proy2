b941cf856b0eccf5eb04538b5efa2f9eebde331d
jdcac <jcacaos1@miumg.edu.gt>
2024-11-04 22:36:47 -0600

    Icono de la empresa en el MDI

--- Codigo/Modulos/Contabilidad/Capa_Vista_Contabilidad/Contabilidad_MDI.Designer.cs
+++ Codigo/Modulos/Contabilidad/Capa_Vista_Contabilidad/Contabilidad_MDI.Designer.cs
@@ -163,10 +163,10 @@
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(451, 219);
+            this.pictureBox1.Location = new System.Drawing.Point(321, 183);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.Size = new System.Drawing.Size(542, 348);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
