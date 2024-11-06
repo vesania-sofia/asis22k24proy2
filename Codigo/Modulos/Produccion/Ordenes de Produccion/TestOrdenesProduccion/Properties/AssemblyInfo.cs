fc960984f2dae7f0cc3a3257df3ead3dc59c4f49
PabloFloresUMG <pcesarfm1123@gmail.com>
2024-11-06 16:25:58 -0600

    Software funcional Sistema de producción y Órdenes de producción

---
+++ Codigo/Modulos/Produccion/Ordenes de Produccion/TestOrdenesProduccion/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestOrdenesProduccion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestOrdenesProduccion")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16dc1692-12e9-4170-8273-ad1dcd4728f4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
